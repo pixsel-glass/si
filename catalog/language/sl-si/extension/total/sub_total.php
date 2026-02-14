42fc693646cb092ec9362199cdf9112cad3f509a
devrvk <devrvk@gmail.com>
2026-02-14 16:00:27 +0300

    fix total

--- catalog/language/sl-si/extension/total/sub_total.php
+++ catalog/language/sl-si/extension/total/sub_total.php
@@ -2,6 +2,6 @@
 //Prevod: Chico: https://www.chico.si (ne odstranjujte tega);
 
 // Text
-$_['text_sub_total'] = 'Skupaj brez DDV';
+$_['text_sub_total'] = 'Skupaj';
 
 //Prevod: Chico: https://www.chico.si (ne odstranjujte tega);
